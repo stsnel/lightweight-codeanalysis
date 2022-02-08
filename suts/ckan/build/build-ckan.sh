92984e9ec4be8d3e22ab5865ccbaa7b257424240
Sietse Snel <sietsesnel@protonmail.com>
2022-02-08 21:25:29 +0100

    ckan build env: add plugins
    Add hierarchy and showcase plugins to expose more GUI endpoints

--- suts/ckan/build/build-ckan.sh
+++ suts/ckan/build/build-ckan.sh
@@ -7,5 +7,6 @@ fi
 cd ckan
 git checkout 2.9.3-testar
 cp ../docker-compose-build.yml contrib/docker/docker-compose.yml
+cp ../ckan-entrypoint.sh contrib/docker/ckan-entrypoint.sh
 cp ../Dockerfile .
 docker build . --no-cache -t 378672356020.dkr.ecr.us-east-1.amazonaws.com/ckan
