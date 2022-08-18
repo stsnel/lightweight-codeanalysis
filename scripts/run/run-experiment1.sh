d6ea3e6435fbe38d18a9c27c261a255ae3a45a58
Sietse Snel <sietsesnel@protonmail.com>
2022-08-18 22:20:35 +0200

    Add initial ver run script exp 1

---
+++ scripts/run/run-experiment1.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# Runs Testar experiment 1
+# 
+# Settings
+# 1. Protocol: should be webdriver_ckan1
+PROTOCOL=webdriver_ckan1
+# 2. Condition (experimental, control-defaultactionselection, control-customactionselection)
+CONDITION=experimental
+# 3. Experiment name
+EXPNAME="test1808"
+# 4. Main data directory
+MAINDATADIR="/data/studie/af/output"
+# 5. NumberOfSequences
+NUMSEQUENCES=2
+# 6. Sequence length (number actions in sequence)
+SEQUENCELENGTH=10
+# 7. Number first run
+FIRSTRUN=1
+# 8. Number last run >= first run
+LASTRUN=2
+# 9. OrientDB settings
+ORIENTDB_USER="root"
+ORIENTDB_PASSWORD="testar"
+ORIENTDB_CONNECTDB="demodb"
+ORIENTDB_TESTARDB="testar"
+
+# --- end of settings
+
+set -x
+
+# Create data main directory if it doesn't exist
+if ! [ -d "$MAINDATADIR" ]
+then mkdir -p "$MAINDATADIR"
+fi
+
+GLOBAL_OPTIONS="sse=$PROTOCOL ShowVisualSettingsDialogOnStartup=false Mode=Generate Sequences=$NUMSEQUENCES SequenceLength=$SEQUENCELENGTH"
+
+# Look up options for experimental condition
+if [ "$CONDITION" == "experimental" ]
+then EXP_OPTIONS="Expcondition=experimental SetLogContext=true ProcessDataAfterAction=true CompoundTextActionLogicEnabled=true"
+elif [ "$CONDITION" == "control-defaultactionselection" ]
+then EXP_OPTIONS="ExpCondition=control-defaultactionselection SetLogContext=false ProcessDataAfterAction=false CompoundTextActionLogicEnabled=false"
+elif [ "$CONDITION" == "control-customactionselection" ]
+then EXP_OPTIONS="ExpCondition=control-customactionselection SetLogContext=false ProcessDataAfterAction=false CompoundTextActionLogicEnabled=true"
+else echo "Error: unknown experimental condition $CONDITION" && exit 1
+fi
+
+for RUN in $(seq "$FIRSTRUN" "$LASTRUN")
+do  DATADIR="$MAINDATADIR/$EXPNAME-$RUN"
+    RUN_OPTIONS="CoverageExportDirectory=$DATADIR OutputDir=$DATADIR"
+    ORIENTDB_EXPORTFILE="$DATADIR/statedb.dump"
+
+    # Create data directory
+    mkdir "$DATADIR"
+
+    # Write settings file
+    cat > "$DATADIR/exp1.settings" << SETTINGSFILE
+    Experiment: $EXPNAME [$RUN]
+    Global options: $GLOBAL_OPTIONS
+    Run options: $RUN_OPTIONS
+SETTINGSFILE
+
+    ## Create state database
+    /opt/orientdb/bin/console.sh "CONNECT remote:localhost/$ORIENTDB_CONNECTDB $ORIENTDB_USER $ORIENTDB_PASSWORD; CREATE DATABASE remote:localhost/$ORIENTDB_TESTARDB $ORIENTDB_USER $ORIENTDB_PASSWORD;"
+
+    ## Run experiment
+    cd  /data/studie/af/TESTAR_dev/testar/target/install/testar/bin || exit 1
+    ./testar $GLOBAL_OPTIONS $EXP_OPTIONS $RUN_OPTIONS
+
+    ## Export state database
+    /opt/orientdb/bin/console.sh "CONNECT remote:localhost/$ORIENTDB_TESTARDB $ORIENTDB_USER $ORIENTDB_PASSWORD; EXPORT DATABASE $ORIENTDB_EXPORTFILE;"
+
+    ## Drop state database
+    /opt/orientdb/bin/console.sh "CONNECT remote:localhost/$ORIENTDB_CONNECTDB $ORIENTDB_USER $ORIENTDB_PASSWORD; DROP DATABASE remote:localhost/$ORIENTDB_TESTARDB $ORIENTDB_USER $ORIENTDB_PASSWORD"
+done
+
+echo "Run script done."
