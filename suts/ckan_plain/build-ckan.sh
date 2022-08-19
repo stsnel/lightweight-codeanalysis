75cb16fda0602fdd4c7af334d365f71593bf73aa
Sietse Snel <sietsesnel@protonmail.com>
2022-08-19 22:47:17 +0200

    Add ckan_plain build config

---
+++ suts/ckan_plain/build-ckan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d ckan ]
+then git clone https://github.com/stsnel/ckan.git
+fi
+
+cd ckan
+git checkout 2.9.3
+cp ../docker-compose-build.yml contrib/docker/docker-compose.yml
+cp ../ckan-entrypoint.sh contrib/docker/ckan-entrypoint.sh
+cp ../Dockerfile .
+docker build . --no-cache -t 378672356020.dkr.ecr.us-east-1.amazonaws.com/ckan_plain
