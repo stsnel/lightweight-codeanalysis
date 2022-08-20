3333efc1ddc3035add7278de863cd2320eb97f0d
Sietse Snel <sietsesnel@protonmail.com>
2022-08-20 15:56:49 +0200

    CKAN SUT script: prune containers

--- suts/ckan/run/up.sh
+++ suts/ckan/run/up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 export UID
+docker container prune -f
 docker image prune -f
 docker volume prune -f
-docker-compose rm -f
 docker-compose pull
 docker-compose up --force-recreate --build -d
