f23b8d540ce64a12e5ecf49dea0fa803416a9a5b
Sietse Snel <sietsesnel@protonmail.com>
2022-09-16 21:23:20 +0200

    process script: time tables add median

--- scripts/summarize/gen-table-time.py
+++ scripts/summarize/gen-table-time.py
@@ -31,27 +31,30 @@ for condition in conditions:
     data = get_data_by_condition(args, condition)
     avg = statistics.mean(data)
     sd = statistics.stdev(data)
+    median = statistics.median(data)
     n = len(data)
     results.append( { "Condition": condition,
                       "Average": str(round(avg/1000,3)),
                       "Standard deviation": str(round(sd/1000,3)),
+                      "Median": str(round(median/1000,3)),
                       "N": str(n) } )
 
 if args.outformat == "csv":
-   fieldnames = ["Condition", "Average", "Standard deviation", "N"]
+   fieldnames = ["Condition", "Average", "Standard deviation", "Median", "N"]
    writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, delimiter = ';')
    writer.writeheader()
    for result in results:
        writer.writerow(result)
 elif args.outformat == "latex":
    print("\\begin{center}")
-   print("\\begin{tabular}{|l|l|l|l|}")
+   print("\\begin{tabular}{|l|l|l|l|l|}")
    print("\hline")
-   print("Condition & Average & Standard deviation & N \\\\")
+   print("Condition & Median & Average & Standard deviation & N \\\\")
    print("\hline")
    for result in results:
-       print( "{} & {} & {} & {} \\\\".format(
+       print( "{} & {} & {} & {} & {} \\\\".format(
            result["Condition"],
+           result["Median"],
            result["Average"],
            result["Standard deviation"],
            result["N"] ))
