896a6c11af65cc987923a84495ea5e6e31b0355a
Sietse Snel <sietsesnel@protonmail.com>
2022-10-09 23:44:49 +0200

    Add script for generating action time table

---
+++ scripts/summarize/gen-table-actiontime.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import sys
+
+import statistics
+
+# This scripts compiles a table in CSV or LaTeX format of the average/median time
+# per action across experimental runs.
+
+def get_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("infile")
+    parser.add_argument("--outformat", "-o", choices = ["csv","latex"], default = "csv")
+    return parser.parse_args()
+
+def get_data_by_condition(args, condition):
+    data = []
+    with open(args.infile, "r") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter =';')
+        for row in reader:
+            if row["Condition"] == condition:
+                data.append( int(row["Runtime"]) / int(row["Actions"]) )
+    return data
+
+args = get_args()
+conditions = ["plain", "experimental", "control-customactionselection", "control-defaultactionselection"]
+results = []
+for condition in conditions:
+    data = get_data_by_condition(args, condition)
+    avg = statistics.mean(data)
+    sd = statistics.stdev(data)
+    median = statistics.median(data)
+    n = len(data)
+    results.append( { "Condition": condition,
+                      "Average": str(round(avg/1000,3)),
+                      "Standard deviation": str(round(sd/1000,3)),
+                      "Median": str(round(median/1000,3)),
+                      "N": str(n) } )
+
+if args.outformat == "csv":
+   fieldnames = ["Condition", "Average", "Standard deviation", "Median", "N"]
+   writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, delimiter = ';')
+   writer.writeheader()
+   for result in results:
+       writer.writerow(result)
+elif args.outformat == "latex":
+   print("\\begin{center}")
+   print("\\begin{tabular}{|l|l|l|l|l|}")
+   print("\hline")
+   print("Condition & Median & Average & Standard deviation & N \\\\")
+   print("\hline")
+   for result in results:
+       print( "{} & {} & {} & {} & {} \\\\".format(
+           result["Condition"],
+           result["Median"],
+           result["Average"],
+           result["Standard deviation"],
+           result["N"] ))
+   print("\hline")
+   print("\end{tabular}")
+   print("\end{center}")
+else:
+    print("Error: unknown output format " + args.outformat)
+    sys.exit(1)
